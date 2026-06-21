ef5b232a651ca1a46cb82ca487310d23e1aa5854
tranhuyen2204 <Tranthithuhuyen2204@gmail.com>
2026-06-21 14:33:02 +0700

    first commit

---
+++ src/main/resources/static/script2.js
@@ -0,0 +1,83 @@
+const leaves = document.querySelectorAll(".leaf");
+
+let currentLeaf = null;
+let offsetX = 0;
+let offsetY = 0;
+let zIndexCounter = 100;
+
+leaves.forEach(leaf => {
+
+    leaf.addEventListener("mousedown", (e) => {
+
+        e.preventDefault();
+
+        currentLeaf = leaf;
+
+        // Đưa lá đang kéo lên trên cùng
+        currentLeaf.style.zIndex = ++zIndexCounter;
+
+        const rect = currentLeaf.getBoundingClientRect();
+
+        offsetX = e.clientX - rect.left;
+        offsetY = e.clientY - rect.top;
+
+    });
+
+});
+
+document.addEventListener("mousemove", (e) => {
+
+    if (!currentLeaf) return;
+
+    const game = document.querySelector(".game");
+    const gameRect = game.getBoundingClientRect();
+
+    let x = e.clientX - offsetX;
+    let y = e.clientY - offsetY;
+
+    // Giới hạn trong khung game
+    x = Math.max(
+        -currentLeaf.offsetWidth / 2,
+        Math.min(
+            x,
+            gameRect.width - currentLeaf.offsetWidth / 2
+        )
+    );
+
+    y = Math.max(
+        -currentLeaf.offsetHeight / 2,
+        Math.min(
+            y,
+            gameRect.height - currentLeaf.offsetHeight / 2
+        )
+    );
+
+    currentLeaf.style.left = x + "px";
+    currentLeaf.style.top = y + "px";
+
+});
+
+document.addEventListener("mouseup", () => {
+
+    currentLeaf = null;
+
+});
+const checkBtn = document.getElementById("checkBtn");
+const answerInput = document.getElementById("answerInput");
+const message = document.getElementById("message");
+const answer = answerInput.value.trim().toLowerCase();
+
+checkBtn.addEventListener("click", () => {
+
+    const answer = answerInput.value.trim().toLowerCase();
+
+    if(answer === "canh tranh"){
+
+        window.location.href = "index3.html";
+
+    }else{
+
+        message.textContent = "❌ Đáp án sai!";
+    }
+
+});
